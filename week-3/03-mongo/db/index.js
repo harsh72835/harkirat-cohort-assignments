4f17924e4a14f4590ab53f236f6d25e9f004a825
Harsh Patel <harshpatel8163@gmail.com>
2024-01-07 00:02:27 +0530

    upstream code from repo

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,27 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+const dbConnection = () => {
+  mongoose
+    .connect(
+      "mongodb+srv://admin:nDJeIzBY1xIZTGXq@cluster0.wt6tbkr.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => {
+      console.log("db connected successfully.");
+    })
+    .catch((err) => {
+      console.log("error while connecting db", err);
+    });
+};
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+  dbConnection,
+};
